7ca2d6eb36d9a16aadffbb1e838d69aa71e6a43e
bouboucollab <biardalaric@gmail.com>
2026-03-21 16:36:14 +0100

    Update Vite config to specify rollup input

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   root: ".",
-  build: { outDir: "dist" }
+  build: { 
+    outDir: "dist",
+    rollupOptions: {
+      input: "./index.html"
+    }
+  }
 });
